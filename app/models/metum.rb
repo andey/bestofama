ef1ea3ebb146611170cc01ebff0ae53dcc109c85
Andrew Wei <andey.wei@gmail.com>
2013-01-01 14:06:51 -0500

    warning: character class has '-' without escape: /[-a-z0-9-_]/

--- app/models/metum.rb
+++ app/models/metum.rb
@@ -13,5 +13,5 @@ class Metum < ActiveRecord::Base
   attr_accessible :name, :value
   validates_presence_of :name, :value
   validates_uniqueness_of :name
-  validates_format_of :name, :with => /[-a-z0-9-_]/
+  validates_format_of :name, :with => /[-a-z0-9\-_]/
 end

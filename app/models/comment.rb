dd04e96e7aa6cef18bd0bc0cc58043d08544de18
Andrew Wei <andey@ymail.com>
2013-01-04 01:17:35 -0500

    add missing relation 'belongs_to :ama' in comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -28,6 +28,7 @@ class Comment < ActiveRecord::Base
 
   attr_accessible :ama_id, :content, :date, :key, :parent_id, :parent_key, :user_id, :karma
   belongs_to :user
+  belongs_to :ama
   validates_presence_of :ama_id, :key, :parent_key, :user_id
   validates_uniqueness_of :key
 end

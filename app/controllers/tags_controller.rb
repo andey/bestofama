f83a139f7f19ec3e889a3297501a63175a8327b2
Andrew Wei <andey@ymail.com>
2013-01-10 02:31:20 -0500

    change default order of tag listing to wikipedia_hits, thn comment_karma

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -12,7 +12,7 @@ class TagsController < ApplicationController
 
   # GET /tag/:tag
   def show
-    @entities = Entity.tagged_with(params[:tag]).order(:updated_at).reverse_order.paginate(:page => params[:page], :per_page => 25)
+    @entities = Entity.tagged_with(params[:tag]).order(:wikipedia_hits, :comment_karma).reverse_order.paginate(:page => params[:page], :per_page => 25)
 
     respond_to do |format|
       format.html

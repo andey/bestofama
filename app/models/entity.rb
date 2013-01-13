2f286fd5e970ebee5bbb98a96ebbd825f26ec5da
Andrew Wei <andey@ymail.com>
2013-01-13 02:46:58 -0500

    paper_trail conflicts with acts-as-taggable 'tag_list' on Entities. Therefore ignore updates on tag_list

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -29,7 +29,7 @@ class Entity < ActiveRecord::Base
   has_and_belongs_to_many :users
   has_many :entities_links, :dependent => :destroy
   acts_as_taggable
-  has_paper_trail :ignore => [:updated_at, :wikipedia_hits, :comment_karma, :link_karma]
+  has_paper_trail :ignore => [:updated_at, :wikipedia_hits, :comment_karma, :link_karma, :tag_list]
   validates_presence_of :name, :slug
   validates_uniqueness_of :slug
 end

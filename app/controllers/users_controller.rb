20a7030b52f98d79c6c51f137b790caa1ff3bc33
Andrew Wei <andey.wei@gmail.com>
2013-02-09 23:50:19 -0500

    Minor Adjustments in Layouts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,10 +7,16 @@ class UsersController < ApplicationController
   # Link to reddit.com user can be found on this page as well.
   def show
     @user = User.find_by_username(params[:username])
-    @amas = @user.amas.order(:date).reverse_order
 
     respond_to do |format|
-      format.html
+      if @user && @user.entities.count == 1
+        format.html { redirect_to entity_path(@user.entities.first.slug), :status => :moved_permanently }
+      elsif @user
+        @amas = @user.amas.order(:date).reverse_order
+        format.html
+      else
+        format.html { redirect_to root_path, :notice => "User does not exist" }
+      end
     end
   end
 end

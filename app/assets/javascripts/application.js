008951f8939dff27e34cb546fddce068d48f2e3f
Andrew Wei <andey.wei@gmail.com>
2013-06-02 21:40:50 -0500

    Remove TurboLinks & Upgrade jQuery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,7 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery-2.0.1.min
+//= require jquery-2.0.2.min
 //= require jquery_ujs
-//= require jquery.turbolinks
 //= require bootstrap-3.0.0.min
 //= require_tree .
-//= require turbolinks

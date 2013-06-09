9b0f4645950088a6d88c3687032a1432ed1176b8
Andrew Wei <andey.wei@gmail.com>
2013-06-09 15:35:57 -0500

    Rename config.secret_key to config.secret_key_base

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,6 @@ module Bestofama
     config.active_support.escape_html_entities_in_json = true
     config.assets.enabled = true
     config.assets.version = '1.0'
-    config.secret_token = 'c1ad4b219d69cca4970a0ab0b2e543a732226274bc655a53383f7cf7e437aebb6c48d629df92401967962ef93add4589cb4f3f81a0e0ad969d00b474c78b5e01'
+    config.secret_key_base = 'c1ad4b219d69cca4970a0ab0b2e543a732226274bc655a53383f7cf7e437aebb6c48d629df92401967962ef93add4589cb4f3f81a0e0ad969d00b474c78b5e01'
   end
 end

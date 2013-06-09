03cb61bc290c7a3f1b4739fb7ab0f6a7fb842f38
Andrew Wei <andey.wei@gmail.com>
2013-06-09 14:54:03 -0500

    Move config.secret_token into application.rb

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,8 @@ module Bestofama
     config.encoding = "utf-8"
     config.filter_parameters += [:password]
     config.active_support.escape_html_entities_in_json = true
-    #config.active_record.whitelist_attributes = true
     config.assets.enabled = true
     config.assets.version = '1.0'
+    config.secret_token = 'c1ad4b219d69cca4970a0ab0b2e543a732226274bc655a53383f7cf7e437aebb6c48d629df92401967962ef93add4589cb4f3f81a0e0ad969d00b474c78b5e01'
   end
 end

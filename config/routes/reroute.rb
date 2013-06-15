0c683e21621981ea7f85afe4e1c4d8256ab55643
Andrew Wei <andey.wei@gmail.com>
2013-06-15 11:09:09 -0500

    Define a few more old routes, and add /cdn-cgi/ to robots disallow

--- config/routes/reroute.rb
+++ config/routes/reroute.rb
@@ -4,12 +4,14 @@ Bestofama::Application.routes.draw do
   get 'entity/:slug' => redirect("/ops/%{slug}")
 
   get 'tag/:tag' => redirect("/tags/%{tag}")
+  get 'directory/:tag' => redirect("/tags/%{tag}")
 
-  get 'r/:subreddit' => redirect('http://www.reddit.com/r/%{subreddit}')
-  get 'u/:username' => redirect('http://www.reddit.com/u/%{username}')
+  get 'r/:subreddit' => redirect('http://anonym.to/?http://www.reddit.com/r/%{subreddit}')
+  get 'u/:username' => redirect('http://anonym.to/?http://www.reddit.com/u/%{username}')
 
   get 'user/:username' => redirect("/users/%{username}")
   get 'user/:username/ama/:key/:slug/' => redirect("/amas/%{key}")
   get ':entity/:key' => redirect('/amas/%{key}')
+  get 'submit' => redirect('/amas/submit')
 
 end

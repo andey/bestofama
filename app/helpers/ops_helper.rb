d709f6dddf5d19a6026c037b970918aa7fc8ce4a
Andrew Wei <andey.wei@gmail.com>
2013-07-28 22:08:26 -0500

    Wikipedia mobile URL helper wasn't expecting https

--- app/helpers/ops_helper.rb
+++ app/helpers/ops_helper.rb
@@ -1,15 +1,8 @@
 module OpsHelper
   def wikipedia_mobile(full_url)
-    match = /http:\/\/en.(.*)/.match(full_url)
+    match = /https?:\/\/en.(.*)/.match(full_url)
     if match[1]
-      return "http://en.m.#{match[1]}"
-    end
-  end
-
-  def twitter_mobile(full_url)
-    match = /https:\/\/(.*)/.match(full_url)
-    if match[1]
-      return "https://mobile.#{match[1]}"
+      return "https://en.m.#{match[1]}"
     end
   end
 end

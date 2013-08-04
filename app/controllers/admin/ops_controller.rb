3a99693b1aa87dde2483a281134d29fba28425bc
Andrew Wei <andey.wei@gmail.com>
2013-08-04 16:04:36 -0500

    Change the order of OPs in Admin Panel

--- app/controllers/admin/ops_controller.rb
+++ app/controllers/admin/ops_controller.rb
@@ -4,7 +4,7 @@ class Admin::OpsController < ApplicationController
 
   # GET /ops
   def index
-    @ops = Op.order(:updated_at).paginate(:page => params[:page], :per_page => 25)
+    @ops = Op.order(:name).paginate(:page => params[:page], :per_page => 25)
 
     respond_to do |format|
       format.html # index.html.erb

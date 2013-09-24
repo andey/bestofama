fd71a8c0b533ab5f7d59f4f3427d0fc903e80c93
Andrew Wei <andey.wei@gmail.com>
2013-09-24 21:17:18 +0000

    Convert DuckDuckGo to HTTParty

--- lib/api/duckduckgo.com.rb
+++ lib/api/duckduckgo.com.rb
@@ -1,24 +1,12 @@
 #== DuckDuckGo Stats API library
 
-module DuckDuckGo
-  require 'net/http'
-  require 'open-uri'
-  require 'override/json'
-  require 'htmlentities'
+class DuckDuckGo
+  include HTTParty
+  base_uri 'api.duckduckgo.com'
 
-  class << self
-
-    # generic http GET request, parses json
-    # @return json
-    def get(url)
-      http = Net::HTTP.new("api.duckduckgo.com")
-      request = Net::HTTP::Get.new(url)
-      response = http.request(request)
-      return JSON.parse(response.read_body)
-    end
-
-    def search(word)
-      return self.get("http://api.duckduckgo.com/?q=#{word.gsub(/-/, '+')}&format=json")
-    end
+  def search(text)
+    options = {:query => {:q => text.gsub(/-/, '+'), :format => 'json'}}
+    response = self.class.get('/', options)
+    return response.code == 200 ? JSON.parse(response.body) : nil
   end
 end

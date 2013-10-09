9c59e19b9a5df1e2f7afe2f433bfb40b3d446493
Andrew Wei <andey.wei@gmail.com>
2013-10-09 23:59:40 +0000

    Refactor Ama#create

--- app/controllers/amas_controller.rb
+++ app/controllers/amas_controller.rb
@@ -52,34 +52,27 @@ class AmasController < ApplicationController
   # will match and accept any url /comment/:key/
   def create
 
-    if !params[:ama_url].empty?
+    # Redirect back to form if no URL was submitted
+    redirect_to(submit_path, :notice => "No URL was submitted") if !params[:ama_url].empty? 
 
-      # Find Reddit.com Key
-      ama_key = params[:ama_url].match('\/comments\/([a-z0-9]{5,6})\/')
+    # Match Reddit.com Key
+    ama_key = params[:ama_url].match('\/comments\/([a-z0-9]{5,6})\/')
 
-      # If a key is found, and the key is not in the trash bin
-      if ama_key && !Trash.find_by_key(ama_key[1])
-
-        # Send to reddit api library to create @ama
-        require 'api/reddit'
-        @ama = Reddit.create_ama(ama_key[1])
-
-        respond_to do |format|
-          if @ama.save
-            format.html { redirect_to ama_path(@ama), :notice => "Thank you for your submission, this AMA has now been put in the process queue." }
-          else
-            @ama = Ama.find_by_key(ama_key[1])
-            format.html { redirect_to ama_path(@ama), :notice => "AMA already added" }
-          end
-        end
-
-        # fallback catch clause if no key was found, or key is in the trash bin
-      else
-        redirect_to submit_path, :notice => "Invalid or Blacklisted AMA"
-      end
+    # Redirect back to form if no key is found, or the key is in the trash bin
+    redirect_to(submit_path, :notice => "Invalid or Blacklisted AMA") if !ama_key || Trash.find_by_key(ama_key[1])
+    
+    # Redirect back to AMA if it already exsits
+    redirect_to(ama_path(@ama), :notice => "AMA already added") if Ama.exists?(key: ama_key)
+    
+    # Fetch AMA    
+    response = Reddit.getAMA(ama_key)
+    if response      
+      @ama = Ama.new
+      @ama = @ama.create_by_json(response[0]["data"]["children"][0]["data"])
+      redirect_to ama_path(@ama), :notice => "Thank you for your submission, this AMA has now been put in the process queue.")
     else
-      redirect_to submit_path, :notice => "No URL was submitted"
+      redirect_to(submit_path, :notice => "Unable to fetch AMA")
     end
-  end
 
+  end
 end

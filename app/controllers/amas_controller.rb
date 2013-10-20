366220f3392a719aa7d68cb0d1f084ea3420d97c
Andrew Wei <andey.wei@gmail.com>
2013-10-20 16:39:09 -0500

    Remove public facing AMA submit form

--- app/controllers/amas_controller.rb
+++ app/controllers/amas_controller.rb
@@ -45,34 +45,4 @@ class AmasController < ApplicationController
       format.html
     end
   end
-
-  # POST /amas
-  #
-  # expects params "ama_url"
-  # will match and accept any url /comment/:key/
-  def create
-
-    # Redirect back to form if no URL was submitted
-    redirect_to(submit_path, :notice => "No URL was submitted") if !params[:ama_url].empty? 
-
-    # Match Reddit.com Key
-    ama_key = params[:ama_url].match('\/comments\/([a-z0-9]{5,6})\/')
-
-    # Redirect back to form if no key is found, or the key is in the trash bin
-    redirect_to(submit_path, :notice => "Invalid or Blacklisted AMA") if !ama_key || Trash.find_by_key(ama_key[1])
-    
-    # Redirect back to AMA if it already exsits
-    redirect_to(ama_path(@ama), :notice => "AMA already added") if Ama.exists?(key: ama_key)
-    
-    # Fetch AMA    
-    response = Reddit.getAMA(ama_key)
-    if response      
-      @ama = Ama.new
-      @ama = @ama.create_by_json(response[0]["data"]["children"][0]["data"])
-      redirect_to ama_path(@ama), :notice => "Thank you for your submission, this AMA has now been put in the process queue."
-    else
-      redirect_to submit_path, :notice => "Unable to fetch AMA"
-    end
-
-  end
 end

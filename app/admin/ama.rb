bf84c3bd5a9d0cee0e9e0732360812165f208286
Andrew Wei <andey.wei@gmail.com>
2013-10-20 16:18:27 -0500

    Automatically prefill OP username

--- app/admin/ama.rb
+++ app/admin/ama.rb
@@ -57,7 +57,7 @@ ActiveAdmin.register Ama do
       end
       row :tag_list
       row :op do
-        link_to ama.user.username, new_admin_op_path(username: ama.user.username)
+        link_to ama.user.username, new_admin_op_path(username: ama.user.username, title: ama.title)
       end
       row :participants do
         ama.users.map{|u| link_to u.username, new_admin_op_path(user_id: u.id)}.join(", ").html_safe
---
+++ app/assets/javascripts/admin/new_op.js.coffee
@@ -0,0 +1,5 @@
+jQuery ->
+  if $('.admin_ops').length > 0
+    $('.users a').click()
+    $('.users input').val(decodeURI((RegExp('username=(.+?)(&|$)').exec(location.search)||[null])[1]))
+    $('#op_name').val(decodeURI((RegExp('title=(.+?)(&|$)').exec(location.search)||[null])[1]))

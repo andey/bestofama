fe22d8359c7e7570b44dad731fbb57d184da3d0b
Andrew Wei <andey.wei@gmail.com>
2013-10-20 21:44:29 -0500

    Active Admin permit Tag params

--- app/admin/tag.rb
+++ app/admin/tag.rb
@@ -1,4 +1,10 @@
 ActiveAdmin.register Tag do
   filter :name
   filter :description
+
+  controller do
+    def permitted_params
+      params.permit(:tag => [:name, :description, :wikipedia_url, :meaningless])
+    end
+  end
 end

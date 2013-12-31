456579565e2d22a3d6d342dd9cfd374cf35240db
Andrew Wei <andey.wei@weathermatic.com>
2013-12-30 21:45:08 -0500

    Update CarrierWave config to allow S3 buckets with subdomain

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -10,6 +10,7 @@ CarrierWave.configure do |config|
       :provider => 'AWS',
       :aws_access_key_id => ENV['S3_KEY'],
       :aws_secret_access_key => ENV['S3_SECRET'],
+      :path_style => true
   }
   config.fog_directory = 's3.bestofama.com'
 end

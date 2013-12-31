2e86cca47f08ad9589761b55fe10243d7a9527cd
Andrew Wei <andey.wei@weathermatic.com>
2013-12-30 22:00:52 -0500

    Add begin/rescue to reddit API requests

--- lib/api/reddit.com.rb
+++ lib/api/reddit.com.rb
@@ -3,17 +3,21 @@
 class Reddit
   include HTTParty
   base_uri 'www.reddit.com'
-  
+
   def get(path)
-    options = {:format => 'json', :headers => {'User-Agent' => 'bestofama.com / andey@reddit.com'}}
-    response = self.class.get(path, options)
-    return response.code == 200 ? JSON.parse(response.body) : nil
+    begin
+      options = {:format => 'json', :headers => {'User-Agent' => 'bestofama.com / andey@reddit.com'}}
+      response = self.class.get(path, options)
+      return response.code == 200 ? JSON.parse(response.body) : nil
+    rescue
+      return nil
+    end
   end
 
   def getAMA(key)
     return Rails.env == 'test' ? JSON.parse(File.read("spec/api/reddit.com/comments/#{key}.json")) : get("/comments/#{key}.json")
   end
-  
+
   def getIAMAs
     return Rails.env == 'test' ? JSON.parse(File.read("spec/api/reddit.com/r/IAmA.json")) : get("/r/IAmA.json")
   end

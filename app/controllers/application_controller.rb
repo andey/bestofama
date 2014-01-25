c9d9ad716416c331126f6d798d6607af4a115b8a
Andrew Wei <andey.wei@gmail.com>
2014-01-25 15:18:03 -0600

    Change my mind, I would rather just raise RoutingError 'Not Found'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  rescue_from "ActionController::UnknownAction", :with => :render_404
-  rescue_from "ActionController::UnknownFormat", :with => :render_404
-  rescue_from "ActionController::RoutingError",  :with => :render_404
+  rescue_from "ActionController::UnknownAction", :with => :raise_404
+  rescue_from "ActionController::UnknownFormat", :with => :raise_404
+  rescue_from "ActionController::RoutingError",  :with => :raise_404
 
   def raise_404
     raise ActionController::RoutingError.new('Not Found')
-  end
-
-  def render_404
-    redirect_to '/404'
   end
 end

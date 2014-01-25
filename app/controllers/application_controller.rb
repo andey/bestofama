5846e94d039ea67d56281e6f8ed87b2f5ce4c52b
Andrew Wei <andey.wei@gmail.com>
2014-01-25 15:15:56 -0600

    #96 - Redirect ActionController::UnknownFormat errors to 404 page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  rescue_from "ActionController::UnknownAction", :with => :render_404
+  rescue_from "ActionController::UnknownFormat", :with => :render_404
+  rescue_from "ActionController::RoutingError",  :with => :render_404
 
   def raise_404
     raise ActionController::RoutingError.new('Not Found')
+  end
+
+  def render_404
+    redirect_to '/404'
   end
 end

13316e536f50f3c0dd7d1d7bde129ee0424677c1
Andrew Wei <andey@ymail.com>
2014-03-30 19:14:21 -0500

    SEO improvements on AMA index

--- app/controllers/amas_controller.rb
+++ app/controllers/amas_controller.rb
@@ -7,11 +7,8 @@ class AmasController < ApplicationController
   # - date (default)
   # - comments count
   # - responses count
-
   def index
-    @order = params[:order] if ['karma', 'comments', 'responses'].include? params[:order]
-    @order ||= 'date'
-    params[:page] ||= 1
+    @order = (['karma', 'comments', 'responses'].include? params[:order]) ? params[:order] : 'date'
     @amas = Ama.where('responses > ?', 0).order(@order).reverse_order.paginate(:page => params[:page], :per_page => 25)
 
     respond_to do |format|
@@ -25,26 +22,15 @@ class AmasController < ApplicationController
   # Show an AMA
   # - Comments are cached and built in the view
   # - @users are users who "participated" (guest speakers) in an AMA.
-
   def show
     @ama = Ama.find_by_key(params[:id]) || raise_404
     @users = @ama.users
     @ops = @ama.ops
-    @changes =  PaperTrail::Version.where(item_type: ['Comment'], item_id: Comment.where(ama_id: @ama).map(&:id)).order(:id).reverse_order
+    @changes = PaperTrail::Version.where(item_type: ['Comment'], item_id: Comment.where(ama_id: @ama).map(&:id)).order(:id).reverse_order
 
     respond_to do |format|
       format.html
     end
   end
 
-  # GET /amas/new
-  #
-  # form to submit an AMA url
-  def new
-    @ama = Ama.new
-
-    respond_to do |format|
-      format.html
-    end
-  end
 end

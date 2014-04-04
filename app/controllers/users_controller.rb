b743b9081d0b8fd7650e337416fc42b0d853d10d
Andrew Wei <andey@ymail.com>
2014-04-03 21:00:57 -0500

    Display more information regarding a user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,8 @@ class UsersController < ApplicationController
   # Link to reddit.com user can be found on this page as well.
   def show
     @user = User.find_by_username(params[:id]) || raise_404
+    @comments = @user.comments.order(:karma).reverse_order
+    @participated = Ama.where(id: @comments.map(&:ama_id)).where.not(id: @user.amas)
 
     respond_to do |format|
       format.html { if @user.belongs_to_only_one_op? then redirect_to op_path(@user.ops.first) end }

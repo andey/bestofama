9dbdb9cff92054f17c46cbf8d16ff73bd4a3a8c8
Andrew Wei <andey@ymail.com>
2014-06-26 17:44:51 -0500

    Fix the controller tests

--- spec/requests/index_spec.rb
+++ spec/requests/index_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe "Index" do
+describe IndexController do
 
   before(:all) do
     10.times do
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,6 +12,8 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 
 RSpec.configure do |config|
+  config.infer_spec_type_from_file_location!
+  config.include Rails.application.routes.url_helpers
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = true
   config.infer_base_class_for_anonymous_controllers = false

bb0a4c13114e6f7d0caf449f285080fc6589d30b
Andrew Wei <andey@ymail.com>
2015-01-25 11:27:55 -0500

    force SSL

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  force_ssl if: :ssl_configured?
   protect_from_forgery
   rescue_from "ActionController::UnknownAction", :with => :raise_404
   rescue_from "ActionController::UnknownFormat", :with => :raise_404
@@ -6,6 +7,10 @@ class ApplicationController < ActionController::Base
 
   def raise_404
     raise ActionController::RoutingError.new('Not Found')
+  end
+
+  def ssl_configured?
+    Rails.env.production?
   end
 
 end
